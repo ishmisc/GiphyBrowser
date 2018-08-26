c569f19a3a249d904598011d648b1ba26b089080
init <init@init.init>
2018-08-26 11:22:39 +0200

    Initial Commit

---
+++ GiphyBrowser/definitions/GiphyDefs.swift
@@ -0,0 +1,15 @@
+//
+//  GiphyDefs.swift
+//  GiphyBrowser
+//
+//  Created by Iurii Shevchuk on 2018-08-26.
+//  Copyright © 2018 Iurii Shevchuk Code reference. All rights reserved.
+//
+
+import Foundation
+
+enum GiphyDefs {
+
+    static let APIKey = GiphyAPIKey
+
+}
---
+++ GiphyBrowser/view-controllers/BrowserViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BrowserViewController.swift
+//  GiphyBrowser
+//
+//  Created by Iurii Shevchuk on 2018-08-26.
+//  Copyright © 2018 Iurii Shevchuk Code reference. All rights reserved.
+//
+
+import UIKit
+
+class BrowserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
